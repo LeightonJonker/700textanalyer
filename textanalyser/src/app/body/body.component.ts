98fbd3a6080c1d4ccd654f167573b6cb2573be06
LeightonJonker <leighton.jonker@gmail.com>
2018-06-12 15:15:44 +1200

    xml working
    trying to extract xml details to form url for image src now

--- textanalyser/src/app/body/body.component.ts
+++ textanalyser/src/app/body/body.component.ts
@@ -8,29 +8,44 @@ import { Component, OnInit } from '@angular/core';
   providers: []
 })
 export class BodyComponent implements OnInit {
+  imgsrc: string = "https://farm"
+  imgfarmid: string = ".staticflickr.com/"
+  final: string
+
   private role: string;
   private  baseurl: string = "https://api.flickr.com/services/rest/";
   private APIkey: string = "0109b289e8e411efba6806edf42383e3";
   private secret: string = "e418eb25616d04f4";
-  private results: string;
   searchterm: string = "dog";
   searchextension: string = "?method=flickr.photos.search&api_key="
   private imagesearchurl: string = this.baseurl+this.searchextension+this.APIkey+"&tags="+this.searchterm;
 
   private flickrsearch(){
+    //var window;
+    var results;
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function processRequest(){
       if (this.readyState == 4 && this.status == 200){
         document.getElementById("demo").innerHTML = this.responseText;
-        this.results = this.responseText;
-        console.log(this.results)
-        if (window.DOMParser){
+        results = this.responseText;
+        console.log(results)
+
           var parser = new DOMParser();
-          var xmlDoc= parser.parseFromString(this.results,  "text/xml");
-        }
-        var x = xmlDoc.getElementsByTagName('photos');
-       // var y = x.nodeValue; // y is the value of the tag 'method' in the xml file. different for search atm can't figure out why
-        console.log("huh " + x.item(0) + " uhu")
+          var xmlDoc= parser.parseFromString(results,  "text/xml");
+        var x = xmlDoc.documentElement.getElementsByTagName("photo");
+
+        var id = x[0].getAttribute("id");
+        var owner = x[0].getAttribute("owner");
+        var secret = x[0].getAttribute("secret");
+        var server = x[0].getAttribute("server");
+        var farm = x[0].getAttribute("farm");
+        var title = x[0].getAttribute("title");
+        var ispublic = x[0].getAttribute("ispublic");
+        var isfriend = x[0].getAttribute("isfriend");
+        var isfamily = x[0].getAttribute("isfamily");
+        // can successfully access saved xml file and get correct tags for image.
+
+        console.log(title)
       }
     };
     xhr.open('GET', this.imagesearchurl, true);
@@ -40,19 +55,18 @@ export class BodyComponent implements OnInit {
 
   // Create a url which is then sent through an https GET request. The return is then turned into an xml file and can be read.
   private flickrtest(){
+   // var window
+    var results
     var atest: string = this.baseurl+"?method=flickr.test.echo&api_key="+this.APIkey;
     console.log (atest)
-   // var users = <HTMLElement>document.getElementById("method")
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function processRequest(){
       if (this.readyState == 4 && this.status == 200){
         document.getElementById("demo").innerHTML = this.responseText;
-        this.results = this.responseText;
-        console.log(this.results)
-        if (window.DOMParser){
+        results = this.responseText;
+        console.log(results)
           var parser = new DOMParser();
-          var xmlDoc= parser.parseFromString(this.results,  "text/xml");
-        }
+          var xmlDoc= parser.parseFromString(results,  "text/xml");
         var x = xmlDoc.getElementsByTagName('method')[0];
         var y = x.childNodes[0]; // y is the value of the tag 'method' in the xml file.
         console.log(y)
